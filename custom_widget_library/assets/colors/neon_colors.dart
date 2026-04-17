be92414783ed5d6784e7372ecc7509286669ff77
zahid-x-dev <zahid.devjourney@gmail.com>
2026-04-17 18:19:05 +0600

    Create neon_colors.dart

---
+++ custom_widget_library/assets/colors/neon_colors.dart
@@ -0,0 +1,10 @@
+class NeonColors {
+  static const neonCyan   = Color(0xFF00F5FF);
+  static const neonPink   = Color(0xFFFF0080);
+  static const neonPurple = Color(0xFF8B00FF);
+  static const neonGreen  = Color(0xFF39FF14);
+  static const neonOrange = Color(0xFFFF6600);
+  static const neonYellow = Color(0xFFFFFF00);
+  static const neonRed    = Color(0xFFFF073A);
+  static const neonBlue   = Color(0xFF1B03A3);
+}
