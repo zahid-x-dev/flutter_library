83398a8cf906424e9c2e6e9df55b64696c464e61
zahid-x-dev <zahid.devjourney@gmail.com>
2026-04-17 18:21:41 +0600

    Create pastel_colors.dart

---
+++ custom_widget_library/assets/colors/pastel_colors.dart
@@ -0,0 +1,10 @@
+class PastelColors {
+  static const pastelPink     = Color(0xFFFFB3C1);
+  static const pastelPurple   = Color(0xFFCDB4DB);
+  static const pastelBlue     = Color(0xFFA2D2FF);
+  static const pastelMint     = Color(0xFFB5EAD7);
+  static const pastelYellow   = Color(0xFFFDFFB6);
+  static const pastelPeach    = Color(0xFFFFCBAA);
+  static const pastelLavender = Color(0xFFE2D9F3);
+  static const pastelSky      = Color(0xFFBDE0FE);
+}
